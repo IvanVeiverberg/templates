c5ec1a1f40f849b5ce9b79bc68427f132247b69b
Ivan Mateus Veiverberg <ivanveiverberg@hotmail.com>
2025-09-17 09:59:41 -0300

    feat: Add DTOs for User and Template

---
+++ src/Templates.Api/DTOs/TemplateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Templates.Api.DTOs
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class TemplateDto
+    {
+        public int? Id { get; set; } 
+
+        [Required]
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ src/Templates.Api/DTOs/UserDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Templates.Api.DTOs
+{
+    public class UserDto
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(200)]
+        public string Email { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string Street { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string City { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Country { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string HouseNumber { get; set; } = string.Empty;
+
+        public string CustomStyle { get; set; } = string.Empty;
+    }
+}
