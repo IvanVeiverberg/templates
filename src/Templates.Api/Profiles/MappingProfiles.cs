ac171ed70309ab1898c1ad4771f5e7a9d6d5ed6d
Ivan Mateus Veiverberg <ivanveiverberg@hotmail.com>
2025-09-17 10:00:26 -0300

    feat: Add AutoMapper configuration with MappingProfiles

---
+++ src/Templates.Api/Profiles/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿namespace Templates.Api.Profiles
+{
+    using AutoMapper;
+    using Templates.Api.DTOs;
+    using Templates.Api.Entities;
+
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<UserDto, User>()
+                .ForMember(user => user.Id, opt => opt.Condition(src => src.Id.HasValue))
+                .ReverseMap();
+
+            CreateMap<TemplateDto, Template>()
+                .ForMember(template => template.Id, opt => opt.Condition(src => src.Id.HasValue))
+                .ReverseMap();
+        }
+    }
+}
